69ea6900e53028244658582c007984550031028c
AnithaGopalakrishnan <sriani21121993@gmail.com>
2024-06-26 15:05:59 +0530

    usaa

---
+++ src/main/java/com/singleton/MyfirstDeployApplication.java
@@ -0,0 +1,13 @@
+package com.singleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singleton/MyfirstDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
